5d6351ac4d77e88576f3b9d833433a93a39d2f07
Marcos Blanquez Gracia <marcosblanquez9@gmail.com>
2026-04-11 10:19:58 +0200

    Feat: Añadidas imagenes a equipos

--- app/consts/players.ts
+++ app/consts/players.ts
@@ -1,6 +1,23 @@
+import AlejandroSanz from '../assets/images/AlejandroSanz.png';
+import JoelRomero from '../assets/JoelRomero.png';
+
 const mockPlayers = [
-  { id_jugador: '1', nombre: 'Joel Romero', edad: 22, dorsal: 18, posicion: 'Lateral Izquierdo', foto_url: 'https://cdn-icons-png.flaticon.com/512/149/149071.png' },
-  { id_jugador: '2', nombre: 'Carlos Pérez', edad: 25, dorsal: 5, posicion: 'Pivote', foto_url: 'https://cdn-icons-png.flaticon.com/512/149/149071.png' },
+    {
+        id_jugador: '1',
+        nombre: 'Joel Romero',
+        edad: 22,
+        dorsal: 18,
+        posicion: 'Lateral Izquierdo',
+        foto_url: JoelRomero
+    },
+    {
+        id_jugador: '2',
+        nombre: 'Alejandro Sanz',
+        edad: 25,
+        dorsal: 66,
+        posicion: 'Pivote',
+        foto_url: AlejandroSanz
+    }
 ];
 
 export default mockPlayers;
